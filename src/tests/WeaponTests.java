2673b170ee515bfe1d796da721b7459249f4a803
David Phillips <dbphillipsnz@gmail.com>
2016-07-19 14:10:54 +1200

    Refactor weapon equality tests

--- src/tests/WeaponTests.java
+++ src/tests/WeaponTests.java
@@ -8,30 +8,34 @@ import org.junit.Test;
 public class WeaponTests {
 
 	@Test
-	public void weaponTypeEquality() {
+	public void typeEquality() {
 		Weapon w = new Weapon(Weapon.Type.CANDLESTICK);
 		assertEquals(Weapon.Type.CANDLESTICK, w.getType());
 	}
 
 	@Test
-	public void weaponTypeEqualityDual() {
+	public void typeEqualityDual() {
 		Weapon w1 = new Weapon(Weapon.Type.ROPE);
 		Weapon w2 = new Weapon(Weapon.Type.ROPE);
 		assertEquals(w1.getType(), w2.getType());
 	}
 
-	/* check equals() method for equality */
+	/* check equals() method for equality, inequality and symmetry */
 	@Test
-	public void weaponEquality() {
-		Weapon w1 = new Weapon(Weapon.Type.ROPE);
-		Weapon w2 = new Weapon(Weapon.Type.ROPE);
-		assertTrue(w1.equals(w2));
-	}
+	public void saneEquals() {
+		Weapon rope1 = new Weapon(Weapon.Type.ROPE);
+		Weapon rope2 = new Weapon(Weapon.Type.ROPE);
+		Weapon candle = new Weapon(Weapon.Type.CANDLESTICK);
 
-	@Test
-	public void weaponInequality() {
-		Weapon w1 = new Weapon(Weapon.Type.CANDLESTICK);
-		Weapon w2 = new Weapon(Weapon.Type.ROPE);
-		assertNotEquals(w1, w2);
+		/* equality to self */
+		assertEquals(rope1, rope1);
+
+		/* symmetric equality across separate objects */
+		assertEquals(rope1, rope2);
+		assertEquals(rope2, rope1);
+
+		/* symmetric inequality */
+		assertNotEquals(rope1, candle);
+		assertNotEquals(candle, rope1);
 	}
 }
