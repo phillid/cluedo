5775afcbc5b0d9b6e89b55e1ff5d0e86fd327fe8
Hamish Brown <brownh.7991@gmail.com>
2016-07-26 11:43:11 +1200

    Remove redundant code from Room

--- src/cluedo/Room.java
+++ src/cluedo/Room.java
@@ -5,13 +5,11 @@ import java.util.HashSet;
 
 public class Room extends Cell {
 	private String name;
-	private Set<PlayerToken> occupants;
 	private Set<WeaponToken> weapons;
 
 	public Room(String name, Cell... neighbours) {
 		super(neighbours);
 		this.name = name;
-		this.occupants = new HashSet<PlayerToken>();
 		this.weapons = new HashSet<WeaponToken>();
 	}
 }
