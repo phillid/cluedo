7dac6caa6d18e53edb12fc7a69bc3f391aa5506c
David Phillips <dbphillipsnz@gmail.com>
2016-07-26 13:20:32 +1200

    Simplify the stuff, subclass PlayerToken and WeaponToken from Token

--- src/cluedo/Token.java
+++ src/cluedo/Token.java
@@ -1,5 +1,19 @@
 package cluedo;
 
 public abstract class Token {
+	String name;
 
+	public Token(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return this.name.equals(((Token)obj).name);
+	}
 }
--- src/cluedo/WeaponToken.java
+++ src/cluedo/WeaponToken.java
@@ -1,45 +1,7 @@
 package cluedo;
 
 public class WeaponToken extends Token {
-	public enum Type {
-		CANDLESTICK,
-		DAGGER,
-		LEAD_PIPE,
-		REVOLVER,
-		ROPE,
-		SPANNER
+	public WeaponToken(String name) {
+		super(name);
 	}
-
-	private Type type;
-
-	public WeaponToken(Type type) {
-		this.type = type;
-	}
-
-	public Type getType() {
-		return this.type;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		WeaponToken other = (WeaponToken) obj;
-		if (type != other.type)
-			return false;
-		return true;
-	}
-
 }
