afa0fcb999af720b569c24562224e5bfc966d21a
David Phillips <dbphillipsnz@gmail.com>
2016-07-29 17:44:48 +1200

    Move textclient stuff into non-static context

--- src/ui/TextClient.java
+++ src/ui/TextClient.java
@@ -5,11 +5,11 @@ import cluedo.Game;
 public class TextClient {
 
 	public TextClient() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) throws Throwable {
 		Game game = new Game();
 		game.start();
+	}
+	
+	public static void main(String[] args) throws Throwable {
+		new TextClient();
 	}
 }
