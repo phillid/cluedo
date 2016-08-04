53f3db0f7b93ebbab33fe9051d4e0ffca53174f4
David Phillips <dbphillipsnz@gmail.com>
2016-08-04 22:06:36 +1200

    More javadoc, gotta get those points

--- src/cluedo/Player.java
+++ src/cluedo/Player.java
@@ -12,7 +12,6 @@ import cluedo.token.PlayerToken;
  * Represents a human player
  *
  */
-
 public class Player {
 	private PlayerToken token;
 	private List<Card> heldCards;
@@ -22,7 +21,11 @@ public class Player {
 	/* false if player made accusation and was wrong */
 	public boolean isPlaying; 
 
-
+	/**
+	 * Basic constructor
+	 * @param token -- PlayerToken that this player is responsible for
+	 * @param heldCards -- List of cards which the player is holding
+	 */
 	public Player(PlayerToken token, List<Card> heldCards) {
 		if (token == null) {
 			throw new IllegalArgumentException("Token must not be null");
@@ -36,30 +39,58 @@ public class Player {
 		isPlaying = true;
 	}
 
+	/**
+	 * Get the List of cards held by this player
+	 * @return List of cards held
+	 */
 	public List<Card> getHeldCards() {
 		return heldCards;
 	}
 
+	/**
+	 * Replace the List of cards held by the player
+	 * @param heldCards -- list of cards to hold
+	 */
 	public void setHeldCards(List<Card> heldCards) {
 		this.heldCards = heldCards;
 	}
 
+	/**
+	 * Get the X value on the board
+	 * @return x value
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Get the Y value on the board
+	 * @return y value
+	 */
 	public int getY() {
 		return y;
 	}
 	
+	/**
+	 * Set the X value
+	 * @param x value
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Set the Y value
+	 * @param y value
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
+	/**
+	 * Get the PlayerToken this player is controlling
+	 * @return PlayerToken of this player
+	 */
 	public PlayerToken getPlayerToken() {
 		return token;
 	}
--- src/cluedo/Position.java
+++ src/cluedo/Position.java
@@ -1,26 +1,52 @@
 package cluedo;
 
+/**
+ * Position class for grouping x and y together
+ * @author David Phillips
+ *
+ */
 public class Position {
 	private int x;
 	private int y;
 	
+	/**
+	 * Bare constructor
+	 * @param x -- x value of the position
+	 * @param y -- y value of the position
+	 */
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Get the X value of the position
+	 * @return x value of position
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Get the Y value of the position
+	 * @return y value of position
+	 */
 	public int getY() {
 		return y;
 	}
 	
+	/**
+	 * Set the X value of the position
+	 * @param x -- x value of position
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Set the Y value of the position
+	 * @param y -- y value of position
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
