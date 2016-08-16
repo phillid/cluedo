534921c418fcc262d32bd5b064906c57fcef91e1
David Phillips <dbphillipsnz@gmail.com>
2016-08-16 14:44:14 +1200

    Begin the gui foo

---
+++ src/ui/GuiClient.java
@@ -0,0 +1,24 @@
+package ui;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import cluedo.Board;
+import cluedo.Game;
+import cluedo.Position;
+import cluedo.cards.Card;
+import cluedo.cards.Deck;
+import cluedo.cards.RoomCard;
+import cluedo.cell.Cell;
+import cluedo.cell.Corridor;
+import cluedo.cell.Doorway;
+import cluedo.cell.Room;
+
+public class GuiClient {
+	public GuiClient() {
+		
+	}
+}
