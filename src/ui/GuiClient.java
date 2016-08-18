29f8b95bfa8cfc026e06bbdc89b3ba9bd355e410
Hamish Brown <brownh.7991@gmail.com>
2016-08-18 13:45:36 +1200

    Add functional player picking dialog

--- src/ui/GuiClient.java
+++ src/ui/GuiClient.java
@@ -3,8 +3,10 @@ package ui;
 import java.awt.BorderLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -20,6 +22,8 @@ import cluedo.cell.Cell;
 import cluedo.cell.Corridor;
 import cluedo.cell.Doorway;
 import cluedo.cell.Room;
+import cluedo.token.PlayerToken;
+import cluedo.token.Token;
 
 public class GuiClient {
 	private JFrame mainWindow;
@@ -43,8 +47,11 @@ public class GuiClient {
 				continue;
 			}
 		} while(playerCount < 3 || playerCount > 6);
-		game = new Game(playerCount);
 		
+		game = new Game(playerCount);
+		for (int i = 0; i < playerCount; i++) {
+			pickPlayer(i+1);
+		}
 		mainWindow   = new JFrame();
 		JPanel contentPanel = new JPanel(new BorderLayout());
 		boardPanel   = new BoardPanel(game);
@@ -76,5 +83,36 @@ public class GuiClient {
 	
 	public static void main(String[] args) {
 		new GuiClient();
+	}
+	
+	public void pickPlayer(int playerNumber) {
+		JPanel optionPanel = new JPanel();
+		optionPanel.setLayout(new BoxLayout(optionPanel,BoxLayout.Y_AXIS));
+		optionPanel.add(new JLabel("Player "+playerNumber+", Enter your name:"));
+		JTextField nameField = new JTextField();
+		optionPanel.add(nameField);
+		optionPanel.add(new JLabel("Pick your token:"));
+		ButtonGroup group = new ButtonGroup();
+		Map<JRadioButton,PlayerToken> buttonMap = new HashMap<>();
+		for (PlayerToken t : game.getAvailablePlayerTokens()) {
+			JRadioButton b = new JRadioButton(t.getName());
+			buttonMap.put(b, t);
+			group.add(b);
+			optionPanel.add(b);
+		}
+		PlayerToken chosenToken = null;
+		while(chosenToken == null) {
+			JOptionPane.showMessageDialog(null,optionPanel);
+			for (Map.Entry<JRadioButton,PlayerToken> e : buttonMap.entrySet()) {
+				if(e.getKey().isSelected()) {
+					chosenToken = e.getValue();
+					break;
+				}
+			}
+			if (chosenToken == null) {
+				JOptionPane.showMessageDialog(null, "Please choose a token!");
+			}
+		}
+		game.addPlayer(nameField.getText(), chosenToken);
 	}
 }
